b08dbca85fab0827fcf3096d440c489cc20d2dbb
Vaughn Dice <vaughn@deis.com>
2016-12-19 14:11:27 -0700

    tests(release_component.go): add createRelease test

--- actions/release_component_test.go
+++ actions/release_component_test.go
@@ -1,6 +1,17 @@
 package actions
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/deis/deisrel/testutil"
+
+	"github.com/google/go-github/github"
+)
 
 func TestGetComponentTitle(t *testing.T) {
 	testCases := map[string]string{
@@ -31,4 +42,62 @@ func TestIsValidSemVerTag(t *testing.T) {
 			t.Errorf("isValidSemVerTag(\"%s\") returned true", tag)
 		}
 	}
+}
+
+func TestCreateRelease(t *testing.T) {
+	ts := testutil.NewTestServer()
+	defer ts.Close()
+
+	component := "controller"
+	title := getComponentTitle(component)
+	sha := "master"
+	newTag := "v1.2.3"
+	body := "changelog"
+	expectedName := "Deis Controller v1.2.3"
+
+	ts.Mux.HandleFunc("/repos/deis/controller/releases", func(w http.ResponseWriter, r *http.Request) {
+		if got := r.Method; got != "POST" {
+			t.Errorf("Request method: %v, want POST", got)
+		}
+		v := new(github.RepositoryRelease)
+		if err := json.NewDecoder(r.Body).Decode(v); err != nil {
+			t.Errorf("decoding %+v into a github.RepositoryRelease failed; Error = %+v", r.Body, err)
+		}
+
+		fmt.Fprintf(w, `{
+			"name": "` + expectedName + `",
+			"tag_name": "` + newTag + `",
+			"target_commitish": "` + sha + `",
+			"body": "` + body + `",
+			"created_at": "0001-01-01T00:00:00Z",
+			"published_at": "0001-01-01T00:00:00Z"
+		}`)
+	})
+
+	got, _, err := createRelease(ts.Client, component, title, sha, newTag, body)
+	if err != nil {
+		t.Errorf("createRelease returned error: %v", err)
+	}
+
+	want := &github.RepositoryRelease{
+		Name: &expectedName,
+		TagName: &newTag,
+		TargetCommitish: &sha,
+		Body: &body,
+		CreatedAt: &github.Timestamp{time.Time{}},
+		PublishedAt: &github.Timestamp{time.Time{}},
+	}
+
+	if !releaseEqual(got, want) {
+		t.Errorf("createRelease returned %+v, want %+v", got, want)
+	}
+}
+
+func releaseEqual(got, want *github.RepositoryRelease) bool {
+	return reflect.DeepEqual(got.Name, want.Name) &&
+		reflect.DeepEqual(got.TagName, want.TagName) &&
+		reflect.DeepEqual(got.TargetCommitish, want.TargetCommitish) &&
+		reflect.DeepEqual(got.Body, want.Body) &&
+		got.CreatedAt.String() == want.CreatedAt.String() &&
+		got.PublishedAt.String() == want.PublishedAt.String()
 }
